078433f28583a344a851ba9d85151c851081770f
hyr <18610612423@163.com>
2016-04-25 16:56:10 +0800

    Initial commit

---
+++ app/src/main/java/com/github/hyr1993/beautifulreader/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.hyr1993.beautifulreader.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.hyr1993.beautifulreader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/github/hyr1993/beautifulreader/view/BaseView.java
@@ -0,0 +1,7 @@
+package com.github.hyr1993.beautifulreader.view;
+
+/**
+ * Created by haohaih on 2016/4/25.
+ */
+public interface BaseView {
+}
